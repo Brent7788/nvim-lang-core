c8b5c7aae18a15f587066f9fa1ba599f8fba8ff9
brent <steyn7788@gmail.com>
2024-02-03 20:52:02 +0200

    More unit tests on comments

--- tests/lib.rs
+++ tests/lib.rs
@@ -50,23 +50,73 @@ pub mod tests {
 
     use crate::Expected;
 
+    const PROJECT_PATH: &str = "/home/brent/Documents/projects";
+
+    const TEST_FILE_PATH: &str = "/nvim-lang-core/tests/file_test_cases";
+    const TEST_COMMENT_PATH: &str = "/comments";
+
     #[tokio::test]
     async fn simple_comment_should_be() {
+        env::set_var("RUST_BACKTRACE", "1");
+
+        let file_path = get_test_comment_path("/simple_one_line_comment.rs");
+
+        let core = NvimLangCore::new(None, None);
+
+        let result = core.process_file(file_path).await;
+
+        log::logger().flush();
+
+        Expected::data_len_to_be(1, &result);
+        Expected::new(1, 10, 15, 6, "simle", vec!["simple", "smile", "simile"]).assert(0, &result);
+    }
+
+    #[tokio::test]
+    async fn multiple_comment_should_be() {
+        env::set_var("RUST_BACKTRACE", "1");
+
+        let file_path = get_test_comment_path("/multiple_comments.rs");
+
+        let core = NvimLangCore::new(None, None);
+
+        let result = core.process_file(file_path).await;
+
+        log::logger().flush();
+
+        Expected::data_len_to_be(6, &result);
+        Expected::new(1, 16, 26, 1, "commmented", vec!["commented"]).assert(0, &result);
+        Expected::new(2, 21, 29, 2, "invoving", vec!["involving", "invoking"]).assert(1, &result);
+        Expected::new(4, 2, 3, 1, "a", vec!["A"]).assert(2, &result);
+        Expected::new(4, 14, 21, 5, "brances", vec!["branches"]).assert(3, &result);
+        Expected::new(4, 38, 47, 2, "especialy", vec!["especially"]).assert(4, &result);
+        Expected::new(4, 79, 85, 2, "prduct", vec!["product"]).assert(5, &result);
+    }
+
+    #[tokio::test]
+    async fn comment_block_should_be() {
         Logger::console_init();
         env::set_var("RUST_BACKTRACE", "1");
 
-        let file_path =
-            "/home/brent/Documents/projects/nvim-lang-core/tests/file_test_cases/comments/simple_one_line_comment.rs";
+        let file_path = get_test_comment_path("/comment_block.rs");
+
         let core = NvimLangCore::new(None, None);
 
-        let result = core.process_file(file_path.to_owned()).await;
+        let result = core.process_file(file_path).await;
 
         info!("{:#?}", result);
 
         log::logger().flush();
 
-        Expected::data_len_to_be(1, &result);
+        Expected::data_len_to_be(6, &result);
+        Expected::new(1, 17, 27, 1, "commmented", vec!["commented"]).assert(0, &result);
+        Expected::new(2, 19, 27, 2, "invoving", vec!["involving", "invoking"]).assert(1, &result);
+        Expected::new(4, 0, 1, 1, "a", vec!["A"]).assert(2, &result);
+        Expected::new(4, 12, 19, 5, "brances", vec!["branches"]).assert(3, &result);
+        Expected::new(4, 36, 45, 2, "especialy", vec!["especially"]).assert(4, &result);
+        Expected::new(4, 77, 83, 2, "prduct", vec!["product"]).assert(5, &result);
+    }
 
-        Expected::new(1, 10, 15, 6, "simle", vec!["simple", "smile", "simile"]).assert(0, &result);
+    fn get_test_comment_path(test_file: &str) -> String {
+        return String::new() + PROJECT_PATH + TEST_FILE_PATH + TEST_COMMENT_PATH + test_file;
     }
 }
