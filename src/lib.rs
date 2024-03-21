a0a65a7e040f96b19fe59943a4ed574d8a89d6d4
brent <steyn7788@gmail.com>
2024-03-21 17:00:43 +0200

    Added language dictionary func to nvim lang lua modules

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,10 @@ use common::logger::Logger;
 use log::{error, info};
 use nvim_oxi::{Dictionary, Function, Object, Result};
 
-use crate::{nvim_lang::NvimLanguageFile, nvim_lang_core::NvimLangCore};
+use crate::{
+    nvim_lang::NvimLanguageFile, nvim_lang_core::NvimLangCore,
+    nvim_lang_dictionary::NvimLanguageDictionary,
+};
 
 pub mod common;
 pub mod lang_tool;
@@ -17,13 +20,14 @@ pub mod programming_lang;
 
 #[nvim_oxi::module]
 fn main() -> Result<Dictionary> {
-    // TODO: Remember to flush the logs
     Logger::file_init(None);
 
     info!("Nvim Language Core Starting...");
 
     let nvim_lang_core = Arc::new(NvimLangCore::new(None, None));
+    let nvim_lang_core_start_processing = nvim_lang_core.clone();
     let nvim_lang_file: Arc<Mutex<Option<NvimLanguageFile>>> = Arc::new(Mutex::new(None));
+    let nvim_language_dictionary = Arc::new(Mutex::new(NvimLanguageDictionary::new()));
     let nvim_lang_file_cp = nvim_lang_file.clone();
 
     let start_processing_fn = move |file_path: String| {
@@ -31,14 +35,14 @@ fn main() -> Result<Dictionary> {
 
         log::logger().flush();
 
-        let tokio_runtime = nvim_lang_core
+        let tokio_runtime = nvim_lang_core_start_processing
             .lang_tool_client
             .tokio_runtime
             .as_ref()
             .unwrap();
 
         let nvim_lang_file = nvim_lang_file.clone();
-        let nvim_lang_core = nvim_lang_core.clone();
+        let nvim_lang_core = nvim_lang_core_start_processing.clone();
 
         tokio_runtime.spawn_blocking(move || {
             let nvim_lang_file_p = nvim_lang_core.process_file(file_path);
@@ -66,7 +70,7 @@ fn main() -> Result<Dictionary> {
         return Result::Ok(());
     };
 
-    let check_process = move |()| {
+    let check_process_fn = move |()| {
         log::logger().flush();
 
         let mut nvim_lang_file_gard = match nvim_lang_file_cp.try_lock() {
@@ -84,15 +88,96 @@ fn main() -> Result<Dictionary> {
         return Result::Ok(nvim_lang_file_dest);
     };
 
+    let nvim_lang_core_add_word = nvim_lang_core.clone();
+    let nvim_language_dictionary_add_word = nvim_language_dictionary.clone();
+
+    let add_word_fn = move |word: String| {
+        let nvim_lang_dictionary = nvim_language_dictionary_add_word.clone();
+        let tokio_runtime = nvim_lang_core_add_word
+            .lang_tool_client
+            .tokio_runtime
+            .as_ref()
+            .unwrap();
+
+        tokio_runtime.spawn_blocking(move || {
+            let mut nvim_language_dictionary_gard = match nvim_lang_dictionary.lock() {
+                Ok(gard) => gard,
+                Err(e) => {
+                    error!(
+                        "Error locking the nvim language dictionary in add word! {:#?}",
+                        e
+                    );
+                    return;
+                }
+            };
+
+            nvim_language_dictionary_gard.append_word(word);
+        });
+
+        return Result::Ok(());
+    };
+
+    let nvim_lang_core_remove_word = nvim_lang_core.clone();
+    let nvim_language_dictionary_remove_word = nvim_language_dictionary.clone();
+
+    let remove_word_fn = move |word: String| {
+        let nvim_lang_dictionary = nvim_language_dictionary_remove_word.clone();
+        let tokio_runtime = nvim_lang_core_remove_word
+            .lang_tool_client
+            .tokio_runtime
+            .as_ref()
+            .unwrap();
+
+        tokio_runtime.spawn_blocking(move || {
+            let mut nvim_language_dictionary_gard = match nvim_lang_dictionary.lock() {
+                Ok(gard) => gard,
+                Err(e) => {
+                    error!(
+                        "Error locking the nvim language dictionary in remove word! {:#?}",
+                        e
+                    );
+                    return;
+                }
+            };
+
+            nvim_language_dictionary_gard.remove_word(word);
+        });
+
+        return Result::Ok(());
+    };
+
+    let nvim_language_dictionary_get_words = nvim_language_dictionary.clone();
+
+    let get_words_fn = move |()| {
+        let nvim_language_dictionary_gard = match nvim_language_dictionary_get_words.lock() {
+            Ok(gard) => gard,
+            Err(e) => {
+                error!(
+                    "Error locking the nvim language dictionary in remove word! {:#?}",
+                    e
+                );
+                return Result::Ok(Vec::new());
+            }
+        };
+
+        return Result::Ok(nvim_language_dictionary_gard.get_words());
+    };
+
     info!("Nvim Language Core has Started");
 
     log::logger().flush();
 
-    let pr = Function::from_fn(start_processing_fn);
-    let cpr = Function::from_fn(check_process);
+    let start_processing_fn = Function::from_fn(start_processing_fn);
+    let check_process_fn = Function::from_fn(check_process_fn);
+    let add_word_fn = Function::from_fn(add_word_fn);
+    let remove_word_fn = Function::from_fn(remove_word_fn);
+    let get_words_fn = Function::from_fn(get_words_fn);
 
     return Ok(Dictionary::from_iter([
-        ("start_processing", Object::from(pr)),
-        ("check_process", Object::from(cpr)),
+        ("start_processing", Object::from(start_processing_fn)),
+        ("check_process", Object::from(check_process_fn)),
+        ("add_word", Object::from(add_word_fn)),
+        ("remove_word", Object::from(remove_word_fn)),
+        ("get_words", Object::from(get_words_fn)),
     ]));
 }
