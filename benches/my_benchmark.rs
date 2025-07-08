196a4102743e43c3559f3658f475503181d051c4
Brent7788 <steyn7788@gmail.com>
2025-07-08 10:45:56 +0800

    Bench mark on version 2

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -4,39 +4,72 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use nvim_lang_core::{
     common::test::{get_bench_path, get_test_comment_path},
     nvim_lang_core::NvimLangCore,
+    nvim_lang_dictionary::NvimLanguageReadonlyDictionary,
+    nvim_language::{self, core::NvimLanguageCore},
 };
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let core = NvimLangCore::new(None, None);
 
-    let mut group = c.benchmark_group("Nvim Lang Bench Programming Lang");
+    let mut group = c.benchmark_group("Nvim Lang Bench");
     let file_path = get_bench_path();
 
     println!("BENCH=== {}", file_path);
     group.measurement_time(Duration::new(80, 0));
     group.sample_size(10);
 
-    group.bench_function("Comment bench", |b| {
+    group.bench_function("Programming Lang bench", |b| {
         b.iter(|| core.process_file(file_path.clone(), None))
     });
 
-    let file_path = "/home/brent/Documents/projects/nvim-lang-core/src/lib.rs".to_owned();
+    let file_path = "/home/brent/Documents/pojects/nvim-lang-core/src/lib.rs".to_owned();
 
     println!("BENCH=== {}", file_path);
     group.measurement_time(Duration::new(30, 0));
     group.sample_size(10);
 
-    group.bench_function("Comment bench lib", |b| {
+    group.bench_function("Bench src/lib.rs", |b| {
         b.iter(|| core.process_file(file_path.clone(), None))
     });
 
-    let file_path = "/home/brent/Documents/projects/nvim-lang-core//tests/fiel_test_cases/rust/codes/multiple_code.rs".to_owned();
+    let file_path = "/home/brent/Documents/pojects/nvim-lang-core/tests/file_test_cases/rust/codes/multiple_code.rs".to_owned();
 
     println!("BENCH=== {}", file_path);
     group.sample_size(20);
 
-    group.bench_function("Comment bench Test", |b| {
+    group.bench_function("Bench Multiple Code Test", |b| {
         b.iter(|| core.process_file(file_path.clone(), None))
+    });
+
+    let core = NvimLanguageCore::new(None, None);
+
+    let file_path = get_bench_path();
+
+    println!("BENCH=== {}", file_path);
+    group.measurement_time(Duration::new(80, 0));
+    group.sample_size(10);
+
+    group.bench_function("Programming Lang bench V2", |b| {
+        b.iter(|| core.process_file(file_path.clone(), NvimLanguageReadonlyDictionary::new()))
+    });
+
+    let file_path = "/home/brent/Documents/pojects/nvim-lang-core/src/lib.rs".to_owned();
+
+    println!("BENCH=== {}", file_path);
+    group.measurement_time(Duration::new(30, 0));
+    group.sample_size(10);
+
+    group.bench_function("Bench src/lib.rs V2", |b| {
+        b.iter(|| core.process_file(file_path.clone(), NvimLanguageReadonlyDictionary::new()))
+    });
+
+    let file_path = "/home/brent/Documents/pojects/nvim-lang-core/tests/file_test_cases/rust/codes/multiple_code.rs".to_owned();
+
+    println!("BENCH=== {}", file_path);
+    group.sample_size(20);
+
+    group.bench_function("Bench Multiple Code Test V2", |b| {
+        b.iter(|| core.process_file(file_path.clone(), NvimLanguageReadonlyDictionary::new()))
     });
 }
 
